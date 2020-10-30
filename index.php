1a166aba51082a8e29c82b938fddca1cb6aead77
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-10-30 12:37:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
   <body>
     <h2><center>ชื่อ รจนา หอมประสิทธิ์</center></h2>
     <h2><center>ชื่อเล่น แป้ง</center></h2>
-    <img src="cat.jpg">
+    <img src="cat.jpg" width="100" height="100">
     <br>
-    <img src="cat3.jpg">
+    <img src="cat3.jpg" width="100" height="100">
     <a href="https://www.it.kmitl.ac.th/th/">This is a Link</a>
   </body>
 </html>
