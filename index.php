ae06bf69be6e00ea503926541e70f2ae46a635ca
64070145Rosjana <73657654+64070145Rosjana@users.noreply.github.com>
2020-10-30 12:24:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <titleWeb Development</title>
+  </head>
+  <body>
+    <h1>ชื่อ รจนา หอมประสิทธิ์</h1>
+    <h1>ชื่อเล่น แป้ง</h1>
+  </body>
+</html>
